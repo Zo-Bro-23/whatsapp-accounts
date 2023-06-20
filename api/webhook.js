b31d2dbdbb1a3bdcecaffa2509cc38e976022130
Zohan Subhash <zohan.subhash@gmail.com>
2023-06-20 16:31:21 +0530

    Commit

--- api/webhook.js
+++ api/webhook.js
@@ -14,18 +14,56 @@ module.exports = async (req, res) => {
         if (req.body?.typeWebhook == 'outgoingMessageReceived' && req.headers?.authorization == `Bearer ${process.env.AUTHOR}` && req.body?.senderData?.chatId == '919288001128@c.us') {
             if (req.body?.messageData?.typeMessage == 'extendedTextMessage') {
                 const message = req.body.messageData.extendedTextMessageData.text
+                await client.connect()
+                const db = client.db(dbName)
+                const collection = db.collection(collectionName)
+
+                if (message == 'Accounts') {
+                    let accounts = []
+                    let response = ''
+                    let total = 0
+                    const documents = await collection.find({}).toArray()
+                    documents.forEach(document => {
+                        if (Date.now() - document._id <= 604800000) {
+                            accounts.push(document)
+                        }
+                    })
+                    accounts.forEach(account => {
+                        total -= account.amount
+                    })
+                    accounts.forEach(account => {
+                        response += `*$${account.amount}:* ${account.description}\n`
+                    })
+
+                    response += `---------------------\n*_Total: $${total}_*`
+
+                    await restAPI.message.sendMessage("919288001128@c.us", null, response)
+                        .catch(error => {
+                            console.log(error.message)
+                            return res.status(400).send(error.message)
+                        })
+                    return res.send('Okay')
+                }
+
                 if (message.slice(0, 1) !== '$') {
                     return res.send('Okay')
                 }
                 const amountIndex = message.indexOf(' ')
+
+                if (amountIndex == -1) {
+                    await restAPI.message.sendMessage("919288001128@c.us", null, `You forgot to add a space!`)
+                        .catch(error => {
+                            console.log(error.message)
+                            return res.status(400).send(error.message)
+                        })
+                    return res.send('Okay')
+                }
                 const amount = message.slice(1, amountIndex)
                 const description = message.slice(amountIndex + 1)
 
-                await client.connect()
-                const db = client.db(dbName)
-                const collection = db.collection(collectionName)
                 await collection.insertOne({ _id: Date.now(), amount, description })
-                await restAPI.message.sendMessage("919288001128@c.us", null, `**Entry inserted: $${amount}**\n(${description})`)
+                
+                await restAPI.message.sendMessage("919288001128@c.us", null, `*Entry added:*\n_Amount:_ $${amount}\n_Description:_ ${description}`)
                     .catch(error => {
                         console.log(error.message)
                         return res.status(400).send(error.message)

373307eff394b45d9b57fa82a4b4d68ae7f3490e
Zohan Subhash <zohan.subhash@gmail.com>
2023-06-20 11:55:27 +0530

    Debug

--- api/webhook.js
+++ api/webhook.js
@@ -1,17 +1,19 @@
 module.exports = (req, res) => {
     const whatsAppClient = require('@green-api/whatsapp-api-client')
+    const TelegramBot = require('node-telegram-bot-api')
     require('dotenv').config()
     const restAPI = whatsAppClient.restAPI({
         idInstance: process.env.ID,
         apiTokenInstance: process.env.KEY
     })
+    const bot = new TelegramBot(process.env.TOKEN)
 
     try {
         console.log(req.body)
         if (req.headers['X-Telegram-Bot-Api-Secret-Token'] == process.env.AUTHOR) {
-            restAPI.message.sendMessage("919288001128@c.us", null, `${req.body.message.text}`)
+            bot.sendMessage("@zobro23", `${req.body.message.text}`)
         }
     } catch (error) {
-        restAPI.message.sendMessage("919288001128@c.us", null, `Accounts error: ${error.message}`)
+        bot.sendMessage("@zobro23", `Accounts error: ${error.message}`)
     }
 }

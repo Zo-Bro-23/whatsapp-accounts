cfa9d6e27fadfdd417442d9e8d686f10262006b3
Zohan Subhash <zohan.subhash@gmail.com>
2023-06-20 11:09:28 +0530

    Fix

--- api/webhook.js
+++ api/webhook.js
@@ -1,10 +1,14 @@
 module.exports = (req, res) => {
     const whatsAppClient = require('@green-api/whatsapp-api-client')
     require('dotenv').config()
-
     const restAPI = whatsAppClient.restAPI({
         idInstance: process.env.ID,
         apiTokenInstance: process.env.KEY
     })
-    restAPI.sendMessage("919288001128@c.us", null, `${JSON.stringify(req.query)} ${JSON.stringify(req.headers)}`)
+
+    try {
+        restAPI.messaage.sendMessage("919288001128@c.us", null, `${JSON.stringify(req.query)} ${JSON.stringify(req.headers)}`)
+    } catch (error) {
+        restAPI.messaage.sendMessage("919288001128@c.us", null, `Accounts error: ${error.message}`)
+    }
 }

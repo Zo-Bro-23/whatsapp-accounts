286c4d0a7cbb3223f43c95fffa46340e7b945231
Zohan Subhash <zohan.subhash@gmail.com>
2023-06-20 12:16:42 +0530

    Debug

--- api/webhook.js
+++ api/webhook.js
@@ -11,7 +11,7 @@ module.exports = (req, res) => {
     try {
         console.log(req.body)
         if (req.body.typeWebhook == 'outgoingMessageReceived' && req.headers.authorization == `Bearer ${process.env.AUTHOR}` && req.body.senderData.chatId == '919288001128@c.us') {
-            restAPI.message.sendMessage("919288001128@c.us", `${req.body.message.text}`)
+            restAPI.message.sendMessage("919288001128@c.us", `${req.body.messageData.extendedTextMessageData.text}`)
         }
     } catch (error) {
         restAPI.message.sendMessage("919288001128@c.us", `Accounts error: ${error.message}`)
